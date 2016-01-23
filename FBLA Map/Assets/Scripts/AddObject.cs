33c67c2c10d4fb300005403ae26de1a6222dc5d1
ASzot <SzotAndrew@live.com>
2016-01-23 02:34:39 -0800

    Added camera functionality, basic unit spawning

---
+++ FBLA Map/Assets/Scripts/AddObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddObject : MonoBehaviour {
+    public GameObject spawnBase;
+
+    public void SpawnObject(string objectName)
+    {
+        //GameObject gameObj = (GameObject)Instantiate(Resources.Load(objectName));
+        GameObject gameObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        gameObj.transform.parent = spawnBase.transform;
+        gameObj.transform.localPosition = new Vector3(10.0f, 10.0f, 0.0f);
+        //gameObj.transform.Translate(30.0f, 5.0f, 0.0f);
+    }
+}
---
+++ FBLA Map/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    private const float SPEED = 10.0f;
+    private const float minFov = 15.0f;
+    private const float maxFov = 90.0f;
+    private const float sensitivity = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(SPEED * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-SPEED * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -SPEED * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, SPEED * Time.deltaTime, 0));
+        }
+
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+    }
+}
