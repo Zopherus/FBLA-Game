b2354cce42113f8b88e789ff23112d5f4e9a2c24
ASzot <SzotAndrew@live.com>
2016-01-25 16:14:16 -0800

    Cleaned the project up a little

---
+++ FBLA Map/Assets/Scripts/Game/CameraBehavior.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour
+{
+
+    ///////////////////////
+    // CONSTANTS
+    ////////////////////
+
+    // Camera movement variables.
+    private const float SPEED = 10.0f;
+    // The minimum field of view.
+    private const float MIN_FOV = 15.0f;
+    // The maximum field of view.
+    private const float MAX_FOV = 90.0f;
+    // The camera sensitivity.
+    private const float CAMERA_SENS = 10.0f;
+    private const float SEL_TIME = 2.0f;
+
+    ///////////////////////////////
+
+    public static Rect Selection = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
+    public Texture2D SelectedHighlight = null;
+    public Texture2D DefaultCursor = null;
+    public Texture2D AttackCursor = null;
+
+    private Vector3 _startClick = -Vector3.one;
+    private bool deselectRect = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+        CheckCameraSelection();
+
+        CheckMoveClick();
+    }
+
+    private void UpdateCursor()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+        Texture2D setTexture = DefaultCursor;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameUnit hitGameUnit = hit.collider.gameObject.GetComponent<GameUnit>();
+            if (hitGameUnit != null && hitGameUnit.Team != 0)
+                setTexture = AttackCursor;
+        }
+
+        float cursorSizeX = setTexture.width;
+        float cursorSizeY = setTexture.height;
+
+        GUI.DrawTexture(new Rect(Event.current.mousePosition.x - (cursorSizeX / 2), Event.current.mousePosition.y - (cursorSizeY / 2), cursorSizeX, cursorSizeY), setTexture);
+        //TODO:
+        // Uncomment this line in production.
+        //Cursor.visible = false;
+    }
+
+    private void CheckMoveClick()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name == "Terrain")
+                {
+                    Vector3 newPos = hit.point;
+
+                    GameObject[] gameObjs = GameObject.FindGameObjectsWithTag("GameUnit");
+                    foreach (GameObject gameObj in gameObjs)
+                    {
+                        GameUnit gameUnit = gameObj.GetComponent<GameUnit>();
+                        if (gameUnit == null)
+                            continue;
+
+                        if (gameUnit.Selected)
+                        {
+                            gameUnit.MoveTo(newPos);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void CheckCameraSelection()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _startClick = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            deselectRect = true;
+        }
+
+        if (Input.GetMouseButton(0))
+            Selection = new Rect(_startClick.x, InvertMouseY(_startClick.y), Input.mousePosition.x - _startClick.x, InvertMouseY(Input.mousePosition.y) - InvertMouseY(_startClick.y));
+    }
+
+    private void OnGUI()
+    {
+        if (deselectRect)
+        {
+            // Deselect the rectangle.
+            _startClick = -Vector3.one;
+            Selection = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
+            deselectRect = false;
+        }
+
+        if (_startClick != -Vector3.one)
+        {
+            GUI.color = new Color(1, 1, 1, 0.5f);
+            GUI.DrawTexture(Selection, SelectedHighlight);
+        }
+
+        UpdateCursor();
+    }
+
+    public static float InvertMouseY(float y)
+    {
+        return Screen.height - y;
+    }
+
+    private void MoveCamera()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(SPEED * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-SPEED * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -SPEED * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, SPEED * Time.deltaTime, 0));
+        }
+
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * CAMERA_SENS;
+        fov = Mathf.Clamp(fov, MIN_FOV, MAX_FOV);
+        Camera.main.fieldOfView = fov;
+    }
+}
---
+++ FBLA Map/Assets/Scripts/Game/GameUnit.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameUnit : MonoBehaviour {
+
+    private const float HEALTH_BAR_WIDTH = 100.0f;
+    private const float HEALTH_BAR_HEIGHT = 5.0f;
+
+    // Variables to be set in the editor.
+    public float UnitSpeed = 10.0f;
+    public float MaxUnitHealth = 100.0f;
+    public Texture2D HealthBarTexture = null;
+
+    public bool Selected = false;
+    // A team of zero is the player.
+    public int Team = 0;
+    public float CurrentUnitHealth = 100.0f;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = UnitSpeed;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetMouseButtonUp(0))
+        {
+            if (CameraBehavior.Selection.width < 0)
+            {
+                CameraBehavior.Selection.x += CameraBehavior.Selection.width;
+                CameraBehavior.Selection.width = -CameraBehavior.Selection.width;
+            }
+            if (CameraBehavior.Selection.height < 0)
+            {
+                CameraBehavior.Selection.y += CameraBehavior.Selection.height;
+                CameraBehavior.Selection.height = -CameraBehavior.Selection.height;
+            }
+            Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position);
+            camPos.y = CameraBehavior.InvertMouseY(camPos.y);
+            Selected = CameraBehavior.Selection.Contains(camPos);
+        }
+        if (Selected)
+            SetSelectedColor(Color.red);
+        else
+            SetSelectedColor(Color.white);
+    }
+
+    void OnGUI()
+    {
+        if (Event.current.type == EventType.Repaint && Selected)
+        {
+            Vector3 shiftedPos = transform.position;
+            shiftedPos.y += 10.0f;
+
+            Vector3 screenSpacePoint = WorldToScreenPoint(shiftedPos);
+            // Display the units health bar. Also draw it centered.
+            float percentageFull = CurrentUnitHealth / MaxUnitHealth;
+            GUI.DrawTexture(new Rect(screenSpacePoint.x - (HEALTH_BAR_WIDTH / 2.0f), screenSpacePoint.y, HEALTH_BAR_WIDTH * percentageFull, HEALTH_BAR_HEIGHT), HealthBarTexture);
+        }
+    }
+
+    public Vector3 WorldToScreenPoint(Vector3 position)
+    {
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(position);
+        screenPosition.y = Screen.height - screenPosition.y;
+        return screenPosition;
+    }
+
+    public void MoveTo(Vector3 positionToMoveTo)
+    {
+        agent.destination = positionToMoveTo;
+    }
+
+    void SetSelectedColor(Color color)
+    {
+        // All the rendering components of the children must be set as well.
+        Renderer[] childrenRenderers = GetComponentsInChildren<Renderer>() as Renderer[];
+        foreach (Renderer childrenRenderer in childrenRenderers)
+            childrenRenderer.material.color = color;
+    }
+}
